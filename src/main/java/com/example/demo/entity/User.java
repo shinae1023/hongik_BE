6c717bed5baccd6b1b690baa34180065fb1677c0
shinae1023 <120792239+shinae1023@users.noreply.github.com>
2025-07-24 13:01:30 +0900

    fix: user entity 수정

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -30,11 +30,15 @@ public class User implements UserDetails {
 
     private String socialId;
     private String address;
+<<<<<<< HEAD
     private String accountNumber;
     private String bank;
 
     private String bank;
     private String accountNumber;
+=======
+    private String account;
+>>>>>>> 4496095 (fix: user entity 수정)
 
     @Enumerated(EnumType.STRING)
     private Role role;
