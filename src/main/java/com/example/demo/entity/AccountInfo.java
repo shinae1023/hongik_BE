346b25ab8b418efd3844cbd6dc70590c1c56704a
alissa159 <seohyunyun325@gmail.com>
2025-07-24 13:23:15 +0900

    [Feat]: 홈 화면 구현

---
+++ src/main/java/com/example/demo/entity/AccountInfo.java
@@ -0,0 +1,17 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AccountInfo {
+    private String bank;
+    private String accountNumber;
+}
