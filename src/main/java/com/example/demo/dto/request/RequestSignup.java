3d01f4fc63cecc99e428171e4dfd6d3e5d937df0
alissa159 <seohyunyun325@gmail.com>
2025-08-07 19:43:05 +0900

    [Feat]: 회원가입 요청 및 서비스 로직 구현

---
+++ src/main/java/com/example/demo/dto/request/RequestSignup.java
@@ -0,0 +1,44 @@
+package com.example.demo.dto.request;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestSignup {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String confirmPassword;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String nickname;
+
+    @NotBlank @Pattern(regexp="^\\d{2,3}-\\d{3,4}-\\d{4}$")
+    private String phone;
+
+    private String bank;
+
+    private String accountNumber;
+
+    @AssertTrue(message = "비밀번호와 비밀번호 확인이 일치하지 않습니다.")
+    private boolean isPasswordsMatch() {
+        if (password == null || confirmPassword == null) return false;
+        return password.equals(confirmPassword);
+    }
+}
---
+++ src/main/java/com/example/demo/service/UserSignupService.java
@@ -0,0 +1,44 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.request.RequestSignup;
+import com.example.demo.entity.Role;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+@RequiredArgsConstructor
+public class UserSignupService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void register(RequestSignup join) {
+
+        if (userRepository.findByEmail(join.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
+        }
+
+        String encodedPassword = passwordEncoder.encode(join.getPassword());
+
+        User user = User.builder()
+                .email(join.getEmail())
+                .password(encodedPassword)
+                .name(join.getName())
+                .nickname(join.getNickname())
+                .phone(join.getPhone())
+                .accountNumber(
+                        StringUtils.hasText(join.getAccountNumber())
+                                ? join.getAccountNumber()
+                                : null
+                )
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+    }
+}
+
