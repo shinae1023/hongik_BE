2822ff33cc11d6503da9051820af6cf23e9378b1
shinae1023 <120792239+shinae1023@users.noreply.github.com>
2025-08-08 01:52:13 +0900

    [Fix] User 엔티티 중복코드 제거

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -36,9 +36,6 @@ public class User extends BaseEntity {
     private String accountNumber;
     private String bank;
 
-    private String bank;
-    private String accountNumber;
-
     @Enumerated(EnumType.STRING)
     private Role role;
 
