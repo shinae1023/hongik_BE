c3a118056f86fd5193b39f4db24d64cb012ac312
alissa159 <seohyunyun325@gmail.com>
2025-08-10 22:56:58 +0900

    [Feat]: 북마크 기능 추가

---
+++ src/main/java/com/example/demo/repository/BookmarkRepository.java
@@ -0,0 +1,20 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Bookmark;
+import com.example.demo.entity.Farm;
+import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    boolean existsByUserAndFarm(User user, Farm farm);
+
+    boolean existsByUserUserIdAndFarmId(Long userId, Long farmId);
+
+    Optional<Bookmark> findByUserAndFarm(User user, Farm farm);
+}
