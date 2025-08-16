e9b9736d7b1675fe079de644d0aa4abf0dbee823
shinae <shinae1023@naver.com>
2025-08-16 13:56:56 +0900

    [Environment] 환경 변수 설정

--- src/main/java/com/example/demo/config/S3Config.java
+++ src/main/java/com/example/demo/config/S3Config.java
@@ -1,15 +1,16 @@
 package com.example.demo.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration // 이 클래스가 설정 파일임을 스프링에게 알려줍니다.
+@Profile("!test")
 public class S3Config {
 
     // application.yml (또는 .properties) 에서 설정한 값들을 가져옵니다.
@@ -24,14 +25,14 @@ public class S3Config {
 
     // AmazonS3Client 객체를 생성하여 Bean으로 등록합니다.
     @Bean
-    public AmazonS3Client amazonS3Client() {
-        // 1. AWS 자격 증명을 생성합니다.
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
-        // 2. 자격 증명과 리전으로 S3 클라이언트를 빌드합니다.
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region)
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
+                )
                 .build();
     }
 }
